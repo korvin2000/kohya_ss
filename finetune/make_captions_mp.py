5ccc8fdb72d122f90f1a53967435b237e44a3195
seoilgun <seoilgun@gmail.com>
2023-08-24 20:30:05 +0900

    test

--- finetune/make_captions_mp.py
+++ finetune/make_captions_mp.py
@@ -279,6 +279,8 @@ def setup_parser() -> argparse.ArgumentParser:
 
 
 if __name__ == "__main__":
+    import multiprocessing as mp
+    mp.set_start_method("spawn", force=True)
     parser = setup_parser()
 
     args = parser.parse_args()

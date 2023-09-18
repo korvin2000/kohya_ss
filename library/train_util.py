aeae1d9c8685e6ae374f84c529f887d973c88b89
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-09-18 19:51:01 +0900

    Update train_util.py

--- library/train_util.py
+++ library/train_util.py
@@ -4403,20 +4403,12 @@ def sample_images_common(
     else:
         if steps % args.sample_every_n_steps != 0 or epoch is not None:  # steps is not divisible or end of epoch
             return
-
     print(f"\ngenerating sample images at step / サンプル画像生成 ステップ: {steps}")
     if not os.path.isfile(args.sample_prompts):
         print(f"No prompt file / プロンプトファイルがありません: {args.sample_prompts}")
         return
-
     org_vae_device = vae.device  # CPUにいるはず
     vae.to(device)
-
-    # read prompts
-
-    # with open(args.sample_prompts, "rt", encoding="utf-8") as f:
-    #     prompts = f.readlines()
-
     if args.sample_prompts.endswith(".txt"):
         with open(args.sample_prompts, "r", encoding="utf-8") as f:
             lines = f.readlines()
@@ -4459,45 +4451,34 @@ def sample_images_common(
 
     if args.v_parameterization:
         sched_init_args["prediction_type"] = "v_prediction"
-
     scheduler = scheduler_cls(
         num_train_timesteps=SCHEDULER_TIMESTEPS,
         beta_start=SCHEDULER_LINEAR_START,
         beta_end=SCHEDULER_LINEAR_END,
         beta_schedule=SCHEDLER_SCHEDULE,
-        **sched_init_args,
-    )
-
+        **sched_init_args,)
     # clip_sample=Trueにする
     if hasattr(scheduler.config, "clip_sample") and scheduler.config.clip_sample is False:
-        # print("set clip_sample to True")
         scheduler.config.clip_sample = True
 
-    pipeline = pipe_class(
-        text_encoder=text_encoder,
-        vae=vae,
-        unet=unet,
-        tokenizer=tokenizer,
-        scheduler=scheduler,
-        safety_checker=None,
-        feature_extractor=None,
-        requires_safety_checker=False,
-        clip_skip=args.clip_skip,
-    )
+    pipeline = pipe_class(text_encoder=text_encoder,
+                            vae=vae,
+                            unet=unet,
+                            tokenizer=tokenizer,
+                            scheduler=scheduler,
+                            safety_checker=None,
+                            feature_extractor=None,
+                            requires_safety_checker=False,
+                            clip_skip=args.clip_skip,)
     pipeline.to(device)
-
     save_dir = args.output_dir + "/sample"
     os.makedirs(save_dir, exist_ok=True)
-
     rng_state = torch.get_rng_state()
     cuda_rng_state = torch.cuda.get_rng_state() if torch.cuda.is_available() else None
-
     with torch.no_grad():
-        # with accelerator.autocast():
         for i, prompt in enumerate(prompts):
             if not accelerator.is_main_process:
                 continue
-
             if isinstance(prompt, dict):
                 negative_prompt = prompt.get("negative_prompt")
                 sample_steps = prompt.get("sample_steps", 30)
@@ -4508,14 +4489,9 @@ def sample_images_common(
                 controlnet_image = prompt.get("controlnet_image")
                 prompt = prompt.get("prompt")
             else:
-                # prompt = prompt.strip()
-                # if len(prompt) == 0 or prompt[0] == "#":
-                #     continue
-
-                # subset of gen_img_diffusers
                 prompt_args = prompt.split(" --")
                 prompt = prompt_args[0]
-                negative_prompt = None
+                negative_prompt = None #args.negative_prompt
                 sample_steps = 30
                 width = height = 512
                 scale = 7.5
@@ -4561,20 +4537,16 @@ def sample_images_common(
                     except ValueError as ex:
                         print(f"Exception in parsing / 解析エラー: {parg}")
                         print(ex)
-
             if seed is not None:
                 torch.manual_seed(seed)
                 torch.cuda.manual_seed(seed)
-
             if prompt_replacement is not None:
                 prompt = prompt.replace(prompt_replacement[0], prompt_replacement[1])
                 if negative_prompt is not None:
                     negative_prompt = negative_prompt.replace(prompt_replacement[0], prompt_replacement[1])
-
             if controlnet_image is not None:
                 controlnet_image = Image.open(controlnet_image).convert("RGB")
                 controlnet_image = controlnet_image.resize((width, height), Image.LANCZOS)
-
             height = max(64, height - height % 8)  # round to divisible by 8
             width = max(64, width - width % 8)  # round to divisible by 8
             print(f"prompt: {prompt}")
@@ -4592,20 +4564,20 @@ def sample_images_common(
                     guidance_scale=scale,
                     negative_prompt=negative_prompt,
                     controlnet=controlnet,
-                    controlnet_image=controlnet_image,
-                )
-
+                    controlnet_image=controlnet_image,)
             image = pipeline.latents_to_image(latents)[0]
 
             ts_str = time.strftime("%Y%m%d%H%M%S", time.localtime())
             num_suffix = f"e{epoch:06d}" if epoch is not None else f"{steps:06d}"
             seed_suffix = "" if seed is None else f"_{seed}"
             img_filename = (
-                f"{'' if args.output_name is None else args.output_name + '_'}{ts_str}_{num_suffix}_{i:02d}{seed_suffix}.png"
-            )
-
-            image.save(os.path.join(save_dir, img_filename))
-
+                f"{'' if args.output_name is None else args.output_name + '_'}{ts_str}_{num_suffix}_{i:02d}{seed_suffix}.png")
+            name, ext = os.path.splitext(img_filename)
+            img_dir = os.path.join(save_dir, img_filename)
+            txt_dir = os.path.join(save_dir, f'{name}.txt')
+            image.save(img_dir)
+            with open(txt_dir, 'w') as f:
+                f.write(prompt)
             # wandb有効時のみログを送信
             try:
                 wandb_tracker = accelerator.get_tracker("wandb")

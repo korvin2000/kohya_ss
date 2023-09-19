4b4307174718a72a32535274a4e6eb845130945f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-09-19 20:07:24 +0900

    Update train_network.py

--- train_network.py
+++ train_network.py
@@ -967,11 +967,11 @@ class NetworkTrainer:
                 saving = (epoch + 1) % args.save_every_n_epochs == 0 and (epoch + 1) < num_train_epochs
                 if is_main_process and saving:
                     ckpt_name = train_util.get_epoch_ckpt_name(args, "." + args.save_model_as, epoch + 1)
-                    accelerator.save_state(ckpt_name)
-                    #save_model(ckpt_name,
+                    #accelerator.save_state(ckpt_name)
+                    save_model(ckpt_name,
                                #accelerator.unwrap_model(network),
-                    #           network,
-                    #           global_step, epoch + 1)
+                               network,
+                               global_step, epoch + 1)
 
                     remove_epoch_no = train_util.get_remove_epoch_no(args, epoch + 1)
                     if remove_epoch_no is not None:
